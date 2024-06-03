09d56c153f35d192c9cf4921fb3ac54a3067e2b3
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-06-03 16:26:57 -0500

    initial-commit

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function Main() {
+  return (
+    <main>
+       <div className="main_imagen_contain">
+        <img src="http://placehold.it/" alt='picy'/>
+       </div>
+       <div className="main_buttons_container">
+        <button className="btns">Tools</button>
+        <button className="btns">Tools</button>
+       </div>
+       <button className="btns">Tools</button>
+    </main>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+    <div className="nav_line"></div>
+    <div className="nav_line"></div>
+    <div className="nav_line"></div>
+    
+
+
+    </nav>
+  )
+}
+
+export default Nav

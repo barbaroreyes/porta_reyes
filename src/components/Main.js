490d3d04d5e2c6986a99eb72b9fef7b9c3e3c08e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-06-03 17:46:47 -0500

    added-links

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,13 +5,13 @@ export default function Main() {
   return (
     <main>
        <div className="main_imagen_contain">
-        <img src="http://placehold.it/" alt='picy'/>
+        <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=2662&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='picy'/>
        </div>
        <div className="main_buttons_container">
         <button className="btns">Tools</button>
-        <button className="btns">Tools</button>
+        <button className="btns">Projects</button>
        </div>
-       <button className="btns">Tools</button>
+       <button className="btns">Email Me</button>
     </main>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Nav() {
   return (
     <nav>
     <div className="nav_line"></div>
     <div className="nav_line"></div>
     <div className="nav_line"></div>
-    
 
+     <ul>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        
+     </ul>
 
     </nav>
   )

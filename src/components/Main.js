b2eb912b6d2d63d4fa0d7de16df4772e7f65b32d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-06-03 19:00:03 -0500

    added-hover-efect

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 export default function Main() {
   return (
@@ -8,10 +8,10 @@ export default function Main() {
         <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=2662&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='picy'/>
        </div>
        <div className="main_buttons_container">
-        <button className="btns">Tools</button>
-        <button className="btns">Projects</button>
+        <Link to="/tools" className="btns">Tools</Link>
+        <Link to="/projects" className="btns">Projects</Link>
        </div>
-       <button className="btns">Email Me</button>
+       <a href="mailto:barbarofreyes@gmail.com" className="btns">Email Me</a>
     </main>
   )
 }

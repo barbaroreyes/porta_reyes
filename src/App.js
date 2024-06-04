830690a36e17dc993fc7628dee697a4212e971f1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-06-03 20:07:58 -0500

    added-products-page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import { Routes ,Route} from  'react-router-dom'
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="container_app">
     <Header/>
-    <Main/>
+    <Routes>
+      <Route path="/"  element={<Main/>} />
+      <Route path="/projects"  element={<Projects/>} />
+
+    </Routes>
+   
     </div>
   );
 }
---
+++ src/pages/Projects.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className="projects_container">
+      
+      <div  className='projects_contatiner-item' >
+        <h3 className='projects_contatiner-title'>Title</h3>
+        <div className='projects_contatiner-image_container'>
+        <img src="https://" alt='pic'/>
+        </div>
+       
+       <div className='projects_contatiner-links'>
+       <a href=""  >App</a>
+        <a href="" >Github</a>
+       </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Projects
